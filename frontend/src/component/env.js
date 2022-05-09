064d01ea92285211fd21397450528c00cc14fd27
Donna61 <donna6197@outlook.com>
2022-05-09 21:09:13 +1000

    new file

---
+++ frontend/src/component/env.js
@@ -0,0 +1,7 @@
+module.exports = {
+    url: function() {
+        return 'http://127.0.0.1:5000'  //example
+        //uncomment the line below and enter replace [URL AND PORT] with your details.  Use the format in the example above
+        // return 'http://[URL AND PORT]'
+    }
+}
