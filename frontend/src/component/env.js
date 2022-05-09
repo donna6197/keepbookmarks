a021ebf9512bca6728c969ea523264c0a0dbf327
Donna61 <donna6197@outlook.com>
2022-05-09 21:39:46 +1000

    minor update to explanatory text

--- frontend/src/component/env.js
+++ frontend/src/component/env.js
@@ -1,17 +1,7 @@
-<<<<<<< HEAD
 module.exports = {
     url: function() {
-        return 'http://127.0.0.1:5000'  // example
+        return 'http://127.0.0.1:5000'  // example entry
         //uncomment the line below and enter replace [URL AND PORT] with your details.  Use the format in the example above
         // return 'http://[URL AND PORT]'
     }
 }
-=======
-module.exports = {
-    url: function() {
-        return 'http://127.0.0.1:5000'  //example
-        //uncomment the line below and enter replace [URL AND PORT] with your details.  Use the format in the example above
-        // return 'http://[URL AND PORT]'
-    }
-}
->>>>>>> fb244e326f1999e27cae02640492c4cb17681aee
