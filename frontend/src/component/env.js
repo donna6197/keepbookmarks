413c6076e260cd3a8799a341149e6a35eca36fbe
Donna61 <donna6197@outlook.com>
2022-05-09 21:14:16 +1000

    formatting change

--- frontend/src/component/env.js
+++ frontend/src/component/env.js
@@ -1,6 +1,6 @@
 module.exports = {
     url: function() {
-        return 'http://127.0.0.1:5000'  //example
+        return 'http://127.0.0.1:5000'  // example
         //uncomment the line below and enter replace [URL AND PORT] with your details.  Use the format in the example above
         // return 'http://[URL AND PORT]'
     }
